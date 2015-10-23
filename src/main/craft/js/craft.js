9cad0bc2eb9f4bb4d8b7975697a19ad2bb7965b1
Jothi Venkataraman <jothi.venkataraman@cerner.com>
2015-10-22 22:35:40 -0500

    Simple GET request handler

--- src/main/craft/js/craft.js
+++ src/main/craft/js/craft.js
@@ -1,6 +1,38 @@
 var craft = angular.module('craft', ['ngRoute']);
 
-craft.controller('mainController', function($scope) {
+craft.controller('mainController', function($scope, mainService) {
+
+    var root = "http://jsonplaceholder.typicode.com";
+
+    function populateUsers(results) {
+        $scope.users = results;
+    }
+
+    function handleError() {
+        alert("An error occurred while processing. Please try again.");
+    }
     // create a message to display in our view
     $scope.message = 'Hello World!';
+
+    $scope.getUsers = function () {
+        mainService.request({
+            method: 'GET',
+            url: root + "/users"
+        }, populateUsers, handleError);
+    }
 });
+
+craft.factory('mainService', ['$http', function ($http) {
+    return {
+        request: function request(reqConfig, onSuccess, onFailure) {
+
+            if (!reqConfig.method) {
+                reqConfig.method = 'GET';
+            }
+
+            reqConfig.params = reqConfig.params || {};
+
+            $http(reqConfig).success(onSuccess).error(onFailure);
+        }
+    }
+}]);
